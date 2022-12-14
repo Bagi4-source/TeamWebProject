6046003ae37355dfbbd07644a2d1dc6eb2c03601
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2022-12-14 17:40:14 +0300

    Добавил карточки поддержки и сделал динамическое изменение их высоты

--- scripts/support.js
+++ scripts/support.js
@@ -1,8 +1,15 @@
-let max_advantage_height = 0;
-$('.advantage').each(function() {
-    max_advantage_height = Math.max($(this).height(), max_advantage_height);
-})
+function draw_advantages(){
+    let max_advantage_height = 0;
+    $('.advantage').each(function() {
+        $(this).css( "height", "");
+        max_advantage_height = Math.max($(this).height(), max_advantage_height);
+    })
 
-$('.advantage').each(function() {
-    $(this).height(max_advantage_height);
-})
+    $('.advantage').each(function() {
+        $(this).height(max_advantage_height);
+    })
+}
+draw_advantages();
+$( window ).resize(function() {
+    draw_advantages();
+});
